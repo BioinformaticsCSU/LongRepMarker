18f0b0f1131dab2db423568aca4b80bc3a122522
XingyuLiao <46807118+XingyuLiao@users.noreply.github.com>
2020-03-09 19:42:25 +0800

    init commit

--- tools/fasta.py
+++ tools/fasta.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 reload(sys)
 fr=open(sys.argv[1],'r')
@@ -17,23 +16,3 @@ for line in fr:
 fw.write('\n')
 fr.close()
 fw.close()
-=======
-import sys
-reload(sys)
-fr=open(sys.argv[1],'r')
-fw=open(sys.argv[2],'w')
-cnt=0
-for line in fr:
-        if line.startswith('>'):
-                if(cnt==0):
-                        fw.write(line)
-                else:
-                        fw.write('\n')
-                        fw.write(line)
-        else:
-                fw.write(line.replace('\n',''))
-        cnt=cnt+1
-fw.write('\n')
-fr.close()
-fw.close()
->>>>>>> fa104b298f252df88828273b9bb3c7eecbb0d259
